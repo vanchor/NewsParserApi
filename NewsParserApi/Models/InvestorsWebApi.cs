5c8bbe0631d8365c6f6e4d7cbc7c038b25bae205
vanchor <vovanyakorev2@gmail.com>
2022-10-05 21:38:27 +0200

    Added dockerfile

---
+++ NewsParserApi/Models/InvestorsWebApi.cs
@@ -0,0 +1,15 @@
+﻿namespace NewsParserApi.Models
+{
+    public class Meta
+    {
+        public int postcount { get; set; }
+        public int totalposts { get; set; }
+        public bool debug { get; set; }
+    }
+
+    public class InvestorsWebApi
+    {
+        public string html { get; set; }
+        public Meta meta { get; set; }
+    }
+}

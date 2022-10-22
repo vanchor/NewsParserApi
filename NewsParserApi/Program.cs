c2aa618b8d21cc84d19c6ce3f1831d258114b52d
vanchor <vovanyakorev2@gmail.com>
2022-10-22 16:21:06 +0200

    Added CORS

--- NewsParserApi/Program.cs
+++ NewsParserApi/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -33,15 +33,26 @@ builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 builder.Services.AddHostedService<TimedNewsParser>();
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins("http://localhost:8081");
+        });
+});
+
 var app = builder.Build();
-     
+
+app.UseCors(MyAllowSpecificOrigins);
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 

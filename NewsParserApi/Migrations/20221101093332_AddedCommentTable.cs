ff850974fb907fd4c1f3162095e4b91b10d49537
vanchor <vovanyakorev2@gmail.com>
2022-11-01 12:43:19 +0100

    Added migrations for db

---
+++ NewsParserApi/Migrations/20221101093332_AddedCommentTable.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsParserApi.Migrations
+{
+    public partial class AddedCommentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_News_NewsId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LikeDislike_News_NewsId",
+                table: "LikeDislike");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NewsId",
+                table: "LikeDislike",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CommentId",
+                table: "LikeDislike",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NewsId",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CommentId",
+                table: "Comments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "username",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LikeDislike_CommentId",
+                table: "LikeDislike",
+                column: "CommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_CommentId",
+                table: "Comments",
+                column: "CommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_username",
+                table: "Comments",
+                column: "username");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Comments_CommentId",
+                table: "Comments",
+                column: "CommentId",
+                principalTable: "Comments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_News_NewsId",
+                table: "Comments",
+                column: "NewsId",
+                principalTable: "News",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Users_username",
+                table: "Comments",
+                column: "username",
+                principalTable: "Users",
+                principalColumn: "Username",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LikeDislike_Comments_CommentId",
+                table: "LikeDislike",
+                column: "CommentId",
+                principalTable: "Comments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LikeDislike_News_NewsId",
+                table: "LikeDislike",
+                column: "NewsId",
+                principalTable: "News",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Comments_CommentId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_News_NewsId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Users_username",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LikeDislike_Comments_CommentId",
+                table: "LikeDislike");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LikeDislike_News_NewsId",
+                table: "LikeDislike");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LikeDislike_CommentId",
+                table: "LikeDislike");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_CommentId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_username",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CommentId",
+                table: "LikeDislike");
+
+            migrationBuilder.DropColumn(
+                name: "CommentId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "username",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NewsId",
+                table: "LikeDislike",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NewsId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_News_NewsId",
+                table: "Comments",
+                column: "NewsId",
+                principalTable: "News",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LikeDislike_News_NewsId",
+                table: "LikeDislike",
+                column: "NewsId",
+                principalTable: "News",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

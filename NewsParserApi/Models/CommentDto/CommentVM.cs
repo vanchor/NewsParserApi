287ee716ada04023ae118d67d82b38e2c1c505ce
vanchor <vovanyakorev2@gmail.com>
2022-11-01 12:41:57 +0100

    Change news Dto

---
+++ NewsParserApi/Models/CommentDto/CommentVM.cs
@@ -0,0 +1,12 @@
+﻿namespace NewsParserApi.Models.CommentDto
+{
+    public class CommentVM
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public string Username { get; set; }
+
+        //public IEnumerable<CommentVM>? Comments { get; set; }
+    }
+}

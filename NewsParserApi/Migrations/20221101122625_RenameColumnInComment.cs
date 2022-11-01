53d6d3c8d6f552e9bb875dc1dd5aedd3669a305a
vanchor <vovanyakorev2@gmail.com>
2022-11-01 13:28:50 +0100

    Migrations for renaming a column in Comment table

---
+++ NewsParserApi/Migrations/20221101122625_RenameColumnInComment.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsParserApi.Migrations
+{
+    public partial class RenameColumnInComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Users_username",
+                table: "Comments");
+
+            migrationBuilder.RenameColumn(
+                name: "username",
+                table: "Comments",
+                newName: "Username");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_username",
+                table: "Comments",
+                newName: "IX_Comments_Username");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Users_Username",
+                table: "Comments",
+                column: "Username",
+                principalTable: "Users",
+                principalColumn: "Username",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Users_Username",
+                table: "Comments");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Comments",
+                newName: "username");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_Username",
+                table: "Comments",
+                newName: "IX_Comments_username");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Users_username",
+                table: "Comments",
+                column: "username",
+                principalTable: "Users",
+                principalColumn: "Username",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
